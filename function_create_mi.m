8fb867696290dd02c640391e3b114b50dfe6915a
midnightgambit777 <113379565+midnightgambit777@users.noreply.github.com>
2024-03-19 20:49:48 -0700

    Delete function_create_mi.m

--- function_create_mi.m
+++
@@ -1,9 +0,0 @@
-function [result,err] = function_create_mi(a,ix)
-   err = 0;
-   result = eye(size(a,1));
-   
-   for i = ix + 1:size(a,1)
-       result(i, ix) = a(i, ix) / a(ix, ix);
-   end 
-end 
-
